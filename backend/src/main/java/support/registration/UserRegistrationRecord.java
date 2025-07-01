e1253fddf7fa5e9f41d05680bfec2dbc29f68d96
sici17 <francescosiciliani624@gmail.com>
2025-07-01 23:55:26 +0200

    Delete backend/src directory

--- backend/src/main/java/support/registration/UserRegistrationRecord.java
+++
@@ -1,6 +0,0 @@
-package support.registration;
-
-import entities.Utente;
-
-public record UserRegistrationRecord(String username, String email, Utente.Sesso sesso, String firstName, String lastName, String password, int id) {
-}
